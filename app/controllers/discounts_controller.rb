ab5243e977884d72a305eb4bdd6af11ec01f8466
Yesi Meza <yesi.meza10@gmail.com>
2021-01-15 23:32:33 -0700

    added show and new actions in controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,13 +1,30 @@
 class DiscountsController < ApplicationController
-  before_action :find_merchant, only: [:index]
+  # before_action :find_merchant, only: [:index]
+  # before_action :find_discount, only: [:show]
   
   def index 
+    @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.discounts
   end
+  # def index 
+  #   @discounts = @merchant.discounts
+  # end
 
-  private 
-
-  def find_merchant
-    @merchant = Merchant.find(params[:merchant_id])
+  def show #if using before acion would just be empty 
+    @discount = Discount.find(params[:id])
   end
+
+  def new 
+    
+  end
+
+  # private 
+
+  # def find_merchant
+  #   @merchant = Merchant.find(params[:merchant_id])
+  # end
+
+  # def find discount
+  #   @discount = Discount.find(params[:id])
+  # end
 end
