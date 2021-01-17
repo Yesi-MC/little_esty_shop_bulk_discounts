0c7d58e8d9f585534053a7c0518d9e238ad489b5
Yesi Meza <yesi.meza10@gmail.com>
2021-01-16 20:35:07 -0700

    added a destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
-    resources :discounts, only: [:index, :show, :new, :create]
+    resources :discounts, only: [:index, :show, :new, :create, :destroy]
   end
 
   namespace :admin do
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   end
 
   # get "/merchant/:id/discounts", to: "merchant_discounts#index"
+  # delete "/merchant/:id/discounts", to: "merchant_discounts#destory"
 
 end
