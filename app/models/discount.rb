3b92a5497f57768a709f0500b1269ec4ec25c234
Yesi Meza <yesi.meza10@gmail.com>
2021-01-18 19:52:36 -0700

    fixed spacing

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,4 @@
 class Discount < ApplicationRecord
-
   validates_presence_of :item_requirement, :percentage_discount
   validates_numericality_of :item_requirement, :in => 1..100000
   validates_numericality_of :percentage_discount, :greater_than_or_equal_to => 0.01, :less_than_or_equal_to => 1
