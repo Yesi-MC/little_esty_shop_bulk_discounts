91eca0aa16d379338b3fe347b0f11379295f6a2f
Yesi Meza <yesi.meza10@gmail.com>
2021-01-18 19:43:26 -0700

    updated validates numericality of

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Discount, type: :model do
   describe "validations" do
     it { should validate_presence_of :item_requirement }
     it { should validate_presence_of :percentage_discount }
+    it { should validate_numericality_of :item_requirement }
+    it { should validate_numericality_of :percentage_discount }
   end 
   describe "relationships" do
     it { should belong_to :merchant }
