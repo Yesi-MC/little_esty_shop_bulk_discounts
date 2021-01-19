864937b65a7e738984a8513af5a9ce9cc0b0e8ad
Yesi Meza <yesi.meza10@gmail.com>
2021-01-18 19:45:02 -0700

    added vlidation of numericality

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,8 @@
 class Discount < ApplicationRecord
+
   validates_presence_of :item_requirement, :percentage_discount
+  validates_numericality_of :item_requirement, :in => 1..100000
+  validates_numericality_of :percentage_discount, :greater_than_or_equal_to => 0.01, :less_than_or_equal_to => 1
+   
   belongs_to :merchant
 end
